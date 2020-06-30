773fd110c658668e9cfa58c5bec42ab54244a0cf
maosan132 <maosan132@gmail.com>
2020-06-30 09:54:23 -0500

    personalized search page

--- bin/main.rb
+++ bin/main.rb
@@ -4,3 +4,19 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 require 'colorize'
+
+# attepmt # 1
+
+html_data = open('http://precios.paternit.com/').read
+nokogiri_object = Nokogiri::HTML(html_data)
+price_elements = nokogiri_object.xpath('')
+
+waterproofing_elements.each do |element|
+  puts element.text
+end
+
+open('http://precios.paternit.com/')
+dom_document = _ # last return or temp_file
+dom_document.read
+
+# attempt # 2
