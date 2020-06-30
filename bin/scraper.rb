9fb8f8e40c6f352a5251dc7af746095fe8bb9e1e
maosan132 <maosan132@gmail.com>
2020-06-30 09:25:53 -0500

    added methods to scraper.rb

--- bin/scraper.rb
+++ bin/scraper.rb
@@ -6,7 +6,13 @@ require 'pry'
 require 'colorize'
 
 class Scraper
-  def parser(_url)
-    Nokogiri::HTML()
+  def data_crawler(url)
+    Nokogiri::HTML(open(url))
+  end
+
+  def get_jobs
+    base_url = ''
+    main_url = "#{base_url}/"
+    data = data_crawler(main_url)
   end
 end
