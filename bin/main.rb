20d01b5e648ee1c023eb1ebbd202d5fdce925bf6
maosan132 <maosan132@gmail.com>
2020-06-30 13:42:46 -0500

    update main.rb: added steps to reproduce problem to solve. Attempts to fine tune nokogiri

--- bin/main.rb
+++ bin/main.rb
@@ -7,6 +7,20 @@ require 'nokogiri'
 require 'pry'
 require 'colorize'
 
+# steps
+
+# 1. Present the script "look for prices by category" that match a category.
+# 2. Give an array of strings to select what kind of category of product client want to know its prices (in the future, it could be any word user wants to search)
+# 2.1 Rspec if is showing the list
+# 3. Take (and validate) the choice and pass it to a method that initializes the crawling
+# 3.1 Rspec if the method accepts and validates the selection
+# 4. Whenever the script finds a match or matches, it would output it in an appropiate form aka "human understandable way". These are the desirable/mandatory outputs:
+#    Title of the categ, the description of product (from where the crawler takes the category name), table of prices with different packaging sizes.
+#    In a future version, instead of showing a table with sizes, user should select what size he wants from an array of choices
+# 4.1 Rspec that script finds a match and output it
+# 5. ask user if wants to make another search
+# 5.1 If user say yes, Rspec that the method restarts the app
+
 # attepmt # 1
 
 # html_data = open('http://precios.paternit.com/').read
@@ -32,4 +46,9 @@ parsed_html.css('.card').css('.card-text').first.inner_text
 parsed_html.css('.card').css('table').first.css('tbody').css('tr').css('td').class.instance_methods.sort
 
 parsed_html.css('.card').css('table').first.css('thead').css('tr').css('th').first.text # gets type of package
-parsed_html.css('.card').css('table').first.css('tbody').css('tr').css('td').first.text # gets named type of package
+puts parsed_html.css('.card').css('table').first.css('tbody').css('tr').css('td').first.text # gets named type of package
+binding.pry
+parsed_html.css('.card').css('table').first.css('tbody').css('tr').css('td').each { |i| puts i }
+parsed_html.css('.card').css('table').first.css('tbody').css('tr').css('td').each { |i| puts i.text }
+
+# each {|i| puts i}
