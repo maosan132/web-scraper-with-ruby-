74eb56a5a16526e04c8c3fe891109f9d5ad898eb
maosan132 <maosan132@gmail.com>
2020-07-07 15:29:21 -0500

    finish main class

--- bin/main.rb
+++ bin/main.rb
@@ -4,7 +4,7 @@
 require_relative '../lib/paternit_scraper.rb'
 require 'open-uri'
 require 'nokogiri'
-require 'pry'
+# require 'pry'
 require 'colorize'
 
 # steps
@@ -37,40 +37,55 @@ title = <<~TITLE
 
 TITLE
 puts title.center(30).colorize(color: :yellow, background: :black).center(90).bold
-
+puts #{user_choice}
 puts 'This app retrieves the next information: product name, usage and prices'
 puts 'from the price list web page of manufacturer company Paternit SA.'
 puts
 puts 'Select a number out of the following categories:'.bold
-print '(1) Adhesives | (2) Waterproofing | (3) Anchor systems | (4) Paints | (5) Cleaners | (6) Sealers | (7) All ' + '?___ '.bold.yellow
-
+puts
+print '(1) Adhesives | (2) Waterproofing | (3) Anchor systems | (4) Paints | (5) Cleaners | (6) Sealers ' + '?___ '.bold.yellow
+puts
 user_choice = gets.chomp.to_i
-
+categories = [ '', 'Adhesives', 'Waterproofing', 'Anchor systems', 'Paints', 'Cleaners', 'sealers']
+selected =  categories[user_choice]
 loop do
-  break if [1, 2, 3, 4, 5, 6, 7].include?(user_choice)
+  break if [1, 2, 3, 4, 5, 6].include?(user_choice)
 
-  puts 'Invalid choice! Please enter one of the following digits: 1 | 2 | 3 | 4 | 5 | 6'
+  print " #{user_choice} is an invalid choice! Please enter one of the following digits: 1 | 2 | 3 | 4 | 5 | 6"
   user_choice = gets.chomp.to_i
 end
 
 terms = case user_choice
         when 1
-          '.card:contains('adhesivo'), .card:contains('pegas'),.card:contains('soldadura'), .card:contains('pegante')'
+          ".card:contains('adhesivo'), .card:contains('pegas'),.card:contains('soldadura'), .card:contains('pegante')"
         when 2
-          '.card:contains('impermeabiliza'), .card:contains('filtraciones'),.card:contains('humedad')'
+          ".card:contains('impermeabiliza'), .card:contains('filtraciones'),.card:contains('humedad')"
         when 3
-          '.card:contains('anclaje')'
+          ".card:contains('anclaje')"
         when 4
-          '.card:contains('vinilo'), .card:contains('esmalte')'
+          ".card:contains('vinilo'), .card:contains('esmalte')"
         when 5
-          '.card:contains('limpia')'
+          ".card:contains('limpia')"
         when 6
-          '.card:contains('sellante'), .card:contains('sellador'),.card:contains('Sellante')'
-        when 7
-          ''
+          ".card:contains('sellante'), .card:contains('sellador'),.card:contains('Sellante')"
   end
 
-# binding.pry
-# puts "choice was: #{terms}"
+  def loader
+    print 'loading ['
+    17.times do
+      sleep 0.025
+      print '.'.light_red
+      sleep 0.025
+      print '.'.light_yellow
+      sleep 0.025
+      print '.'.light_blue
+    end
+    puts ']'
+    sleep 1
+  end
+puts "Your choice was: #{user_choice}, which will search items in category " + "#{selected}:".yellow
+loader
+puts
 new_scraper = PaternitScraper.new
 new_scraper.scraper(terms)
+puts
