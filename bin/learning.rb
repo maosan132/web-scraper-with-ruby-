c1db8a7a5d522d857f9f06efd9f5e9a542c3272b
maosan132 <maosan132@gmail.com>
2020-07-06 19:26:25 -0500

    done with the prototype of desired end result

--- bin/learning.rb
+++ bin/learning.rb
@@ -25,31 +25,33 @@ titles_of_every_match = data.css('.card-title').map(&:text)
 paragraphs_of_every_match = data.css('p').map(&:text)
 tables_of_every_match = data.css('table')
 info = { titles: titles_of_every_match, paragraphs: paragraphs_of_every_match, tables: tables_of_every_match }
-puts "These are the matches: #{titles_of_every_match}"
-# puts info[:paragraphs][1]
-# puts info[:tables][1]
 
-puts " #{matches} products matches your search:".red
+def line
+  puts '-' * 85
+end
+puts
+puts "#{matches} products matches your search:  #{titles_of_every_match}"
 (0..matches - 1).each do |i|
-  puts price_rows = info[:tables][i].css('tr').count - 1
-  puts '<' * 30
-  puts "#{i}. #{info[:titles][i]}"
-  puts '-' * 30
+  puts
+  price_rows = info[:tables][i].css('tr').count - 1
+  puts "#{i+1}. #{info[:titles][i]}".center(4).red.bold
+  line
   puts info[:paragraphs][i]
-  puts '-' * 30
-  puts 'Price List'
-  puts '-' * 30
-  a = info[:tables][i].css('th[1]').text.upcase.center(12).bold
-  b = info[:tables][i].css('th[2]').text.upcase.center(12).bold
+  line
+  puts 'Price List'.red
+  line
+  a = info[:tables][i].css('th[1]').text.upcase.center(12).white.on_red.bold
+  b = info[:tables][i].css('th[2]').text.upcase.center(12).white.on_red.bold
   puts "     #{a}|#{b}"
   c = info[:tables][i].css('td[1]').map { |i| i.text.center(12) }
   d = info[:tables][i].css('td[2]').map { |i| i.text.center(12) }
   (1..price_rows-1).each do |i|
-    puts "     #{c[i]}|#{d[i].colorize(color: :light_blue, background: :black)}\n"
+        puts "     #{c[i].underline}|#{d[i].underline}\n"
   end
   price_rows = 0
+  puts
 end
-puts '-' * 30
+
 
 # data.at_css('tbody').css('tr').each do |row|
 #
